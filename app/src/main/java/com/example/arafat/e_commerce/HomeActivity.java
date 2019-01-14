5d7ce302be81fd8a55d00f85d5abe6ed7fa6cfd9
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-14 22:58:37 +0600

    work with products details activity 2

--- app/src/main/java/com/example/arafat/e_commerce/HomeActivity.java
+++ app/src/main/java/com/example/arafat/e_commerce/HomeActivity.java
@@ -102,12 +102,23 @@ public class HomeActivity extends AppCompatActivity
         FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new
                 FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
 
                         holder.txtProductName.setText(model.getPname());
                         holder.txtProductDescription.setText(model.getDescription());
                         holder.txtProductPrice.setText("Price: " + model.getPrice() + "$");
                         Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        // setting item click listener
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent(HomeActivity.this, ProductDetailsActivity.class);
+                                intent.putExtra("pid", model.getPid());
+                                startActivity(intent);
+                            }
+                        });
 
                     }
 
--- app/src/main/java/com/example/arafat/e_commerce/ProductDetailsActivity.java
+++ app/src/main/java/com/example/arafat/e_commerce/ProductDetailsActivity.java
@@ -1,19 +1,30 @@
 package com.example.arafat.e_commerce;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.example.arafat.e_commerce.Model.Products;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
-    private FloatingActionButton addToCartBtn;
+    private FloatingActionButton addToCartBtn, backButton;
     private ImageView productImage;
     private ElegantNumberButton numberButton;
     private TextView productPrice, productDescription, productName;
+    private String productID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +37,58 @@ public class ProductDetailsActivity extends AppCompatActivity {
         productName = findViewById(R.id.product_name_details);
         productDescription = findViewById(R.id.product_description_details);
         productPrice = findViewById(R.id.product_price_details);
+        backButton = findViewById(R.id.back_button);
+
+        productID = getIntent().getStringExtra("pid");
+
+
+        getProductDetails(productID);
+
+
+        addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String numOfProducts = numberButton.getNumber();
+                Toast.makeText(ProductDetailsActivity.this, numOfProducts + " items have been added to cart",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProductDetailsActivity.this, HomeActivity.class));
+            }
+        });
+
+    }
+
+    private void getProductDetails(String productID) {
+
+        DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        productsRef.child(productID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+                    Products products = dataSnapshot.getValue(Products.class);
+
+                    productName.setText(products.getPname());
+                    productDescription.setText(products.getDescription());
+                    productPrice.setText(products.getPrice());
+                    Picasso.get().load(products.getImage()).into(productImage);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
