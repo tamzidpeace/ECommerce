b80453e1c174c8e3feb86c604593c2ab02287d0b
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-14 23:31:02 +0600

    add item to cart

--- app/src/main/java/com/example/arafat/e_commerce/ProductDetailsActivity.java
+++ app/src/main/java/com/example/arafat/e_commerce/ProductDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.arafat.e_commerce;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,12 +12,19 @@ import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.arafat.e_commerce.Model.Products;
+import com.example.arafat.e_commerce.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
@@ -50,8 +58,54 @@ public class ProductDetailsActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 String numOfProducts = numberButton.getNumber();
-                Toast.makeText(ProductDetailsActivity.this, numOfProducts + " items have been added to cart",
-                        Toast.LENGTH_SHORT).show();
+                /*Toast.makeText(ProductDetailsActivity.this, numOfProducts + " items have been added to cart",
+                        Toast.LENGTH_SHORT).show();*/
+
+                String saveCurrentTime, saveCurrentDate;
+
+                Calendar calForDate = Calendar.getInstance();
+                SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+                saveCurrentDate = currentDate.format(calForDate.getTime());
+
+                SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+                saveCurrentTime = currentTime.format(calForDate.getTime());
+
+                final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+                final HashMap<String, Object> cartMap = new HashMap<>();
+
+                cartMap.put("pid", productID);
+                cartMap.put("pname", productName.getText().toString().trim());
+                cartMap.put("price", productPrice.getText().toString().trim());
+                cartMap.put("date", saveCurrentDate);
+                cartMap.put("time", saveCurrentTime);
+                cartMap.put("quantity", numOfProducts);
+                cartMap.put("discount", "N/A");
+
+                cartListRef.child("User View").child(Prevalent.currentOnlineUser.getPhone())
+                        .child("Products").child(productID)
+                        .updateChildren(cartMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                if (task.isSuccessful()) {
+
+                                    cartListRef.child("Admin View").child(Prevalent.currentOnlineUser.getPhone())
+                                            .child("Products").child(productID)
+                                            .updateChildren(cartMap)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+
+                                                    if (task.isSuccessful()) {
+                                                        Toast.makeText(ProductDetailsActivity.this, "Product Has been added to cart", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }
+                                            });
+                                }
+                            }
+                        });
             }
         });
 
