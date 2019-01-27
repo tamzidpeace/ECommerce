5536a13ea29c1315347e8681b70519600556c741
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-27 13:08:05 +0600

    work with admin section

--- app/src/main/java/com/example/arafat/e_commerce/AdminCategoryActivity.java
+++ app/src/main/java/com/example/arafat/e_commerce/AdminCategoryActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView tShirts, sportsTShirts, femaleDresses, sweathers;
     private ImageView glasses, hatsCaps, walletsBagsPurses, Shoes;
     private ImageView headPhonesHandFree, Laptops, watches, mobilePhones;
+    private Button LogoutBtn, CheckOrderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +31,25 @@ public class AdminCategoryActivity extends AppCompatActivity {
         Laptops = findViewById(R.id.laptop_pc);
         watches = findViewById(R.id.watches);
         mobilePhones = findViewById(R.id.mobile_phone);
+        LogoutBtn = findViewById(R.id.admin_logout_btn);
+        CheckOrderBtn = findViewById(R.id.check_orders_btn);
 
+        CheckOrderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminCategoryActivity.this, AdminNewOrder.class));
+            }
+        });
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
         tShirts.setOnClickListener(new View.OnClickListener() {
@@ -151,8 +171,6 @@ public class AdminCategoryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
 
 
     }
