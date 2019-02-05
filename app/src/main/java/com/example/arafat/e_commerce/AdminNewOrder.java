1b3ea7b3b27d59da0fb040b18cf69ad48e034143
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-02-05 11:18:08 +0600

    31 - displaying orders products

--- app/src/main/java/com/example/arafat/e_commerce/AdminNewOrder.java
+++ app/src/main/java/com/example/arafat/e_commerce/AdminNewOrder.java
@@ -1,15 +1,18 @@
 package com.example.arafat.e_commerce;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.arafat.e_commerce.Model.AdminOrders;
 import com.example.arafat.e_commerce.Prevalent.Prevalent;
@@ -22,6 +25,7 @@ public class AdminNewOrder extends AppCompatActivity {
 
     private RecyclerView orderList;
     private DatabaseReference orderListRef;
+    private static final String TAG = "AdminNewOrder";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +51,25 @@ public class AdminNewOrder extends AppCompatActivity {
         FirebaseRecyclerAdapter<AdminOrders, AdminOrdersViewHolder> adapter =
                 new FirebaseRecyclerAdapter<AdminOrders, AdminOrdersViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull AdminOrdersViewHolder holder, int position, @NonNull AdminOrders model) {
+                    protected void onBindViewHolder(@NonNull AdminOrdersViewHolder holder, final int position, final @NonNull AdminOrders model) {
                         holder.userName.setText(model.getName());
                         holder.userPhoneNumber.setText(model.getPhone());
                         holder.userTotalPrice.setText(model.getTotalAmount());
                         holder.userDateTime.setText(model.getTime() + " " + model.getDate());
                         holder.userShippingAddress.setText(model.getAddress());
+
+                        holder.ShowOrdersBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String uID = getRef(position).getKey();
+                                Intent intent = new Intent(AdminNewOrder.this, AdminUserProduct.class);
+                                intent.putExtra("uid",uID);
+                                //Log.d(TAG, "onClick: " + uID);
+                                //Toast.makeText(AdminNewOrder.this, uID, Toast.LENGTH_SHORT).show();
+                                startActivity(intent);
+
+                            }
+                        });
                     }
 
                     @NonNull
@@ -81,6 +98,7 @@ public class AdminNewOrder extends AppCompatActivity {
             userTotalPrice = itemView.findViewById(R.id.order_product_price);
             userShippingAddress = itemView.findViewById(R.id.order_address_city);
             userDateTime = itemView.findViewById(R.id.order_date_time);
+            ShowOrdersBtn = itemView.findViewById(R.id.show_orders_btn);
         }
     }
 }
---
+++ app/src/main/java/com/example/arafat/e_commerce/AdminUserProduct.java
@@ -0,0 +1,75 @@
+package com.example.arafat.e_commerce;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.arafat.e_commerce.Model.Cart;
+import com.example.arafat.e_commerce.ViewHolder.CartViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AdminUserProduct extends AppCompatActivity {
+
+    private RecyclerView productList;
+    RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference cartListRef;
+    private String userID = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_user_product);
+
+        userID = getIntent().getStringExtra("uid");
+        productList = findViewById(R.id.product_list);
+        productList.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        productList.setLayoutManager(layoutManager);
+
+        cartListRef = FirebaseDatabase.getInstance().getReference()
+                .child("Cart List")
+                .child("Admin View")
+                .child(userID)
+                .child("Products");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Cart> options = new FirebaseRecyclerOptions.Builder<Cart>()
+                .setQuery(cartListRef, Cart.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Cart, CartViewHolder> adapter = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, @NonNull Cart model) {
+
+                holder.txtProductQuantity.setText("Amount: " + model.getQuantity());
+                holder.txtProductPrice.setText("Price per amount: " + model.getPrice() + "$");
+                holder.txtProductName.setText(model.getPname());
+
+            }
+
+            @NonNull
+            @Override
+            public CartViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cart_items_layout, viewGroup, false);
+                CartViewHolder holder = new CartViewHolder(view);
+                return holder;
+            }
+        };
+
+        productList.setAdapter(adapter);
+        adapter.startListening();
+    }
+}
